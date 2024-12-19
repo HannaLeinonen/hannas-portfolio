d78ed91025c378ee2c0c3d45b52b23b53c8e135c
HannaL <hanna.leinonen@iths.se>
2024-12-19 13:42:26 +0100

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+
+require("dotenv").config();
+
+module.exports = {
+  siteMetadata: {
+    title: `Hannas Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
+      "spaceId": process.env.CONTENTFUL_SPACE_ID
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
