e14f30b13a5b157d93b1d5754ce8d7c8725f0612
HannaL <hanna.leinonen@iths.se>
2025-01-07 16:08:46 +0100

    edited settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,25 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
-  siteMetadata: {
-    title: `Hannas Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": process.env.CONTENTFUL_SPACE_ID
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
-};
+    siteMetadata: {
+        title: `Hannas Portfolio`,
+        siteUrl: `https://www.yourdomain.tld`
+    },
+    plugins: [
+        'gatsby-transformer-remark',
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+                spaceId: process.env.CONTENTFUL_SPACE_ID
+            }
+        },
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-postcss'
+    ]
+}
