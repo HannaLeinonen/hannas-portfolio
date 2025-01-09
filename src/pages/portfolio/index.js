3a2bddbda9fce98b20b113f3f923cc8ab316450a
HannaL <hanna.leinonen@iths.se>
2025-01-09 12:32:37 +0100

    small fixes

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,38 +1,39 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 
-const PortfolioPage = () => {
-    const data = useStaticQuery(graphql`
-		query {
-			allContentfulPortfolioItem {
-				nodes {
-					title
-					slug
-				}
-			}
-		}
-	`);
-
-    const items = data.allContentfulPortfolioItem.nodes;
+const PortfolioPage = ({ data }) => {
+    const items = data.allContentfulPortfolioItem.nodes
 
     return (
         <Layout>
             <div>
                 <h1>Portfolio page</h1>
-            <ul>
-                {items.map((item) => (
-                    <li key={item.slug}>
-                        <Link to={`/portfolio/${item.slug}`}>{item.title}</Link>
-                    </li>
-                ))}
-            </ul>
+                <ul>
+                    {items.map((item) => (
+                        <li key={item.slug}>
+                            <Link to={`/portfolio/${item.slug}`}>
+                                {item.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </Layout>
     )
 }
 
+export const query = graphql`
+    query {
+        allContentfulPortfolioItem {
+            nodes {
+                title
+                slug
+            }
+        }
+    }
+`
 export default PortfolioPage
 
 export const Head = () => <Seo title="Portfolio"></Seo>
