092ccf9b46c42f8ad0d08a5a6ac16e138a0e1471
HannaL <hanna.leinonen@iths.se>
2025-01-09 13:24:20 +0100

    small fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
-import { graphql, useStaticQuery, Link } from 'gatsby'
+/* import { graphql, useStaticQuery, Link } from 'gatsby' */
 
 const IndexPage = () => {
-    const data = useStaticQuery(graphql`
+    /* const data = useStaticQuery(graphql`
         query {
             allContentfulPortfolioItem {
                 nodes {
@@ -13,18 +13,12 @@ const IndexPage = () => {
                 }
             }
         }
-    `)
-const projects = data.allContentfulPortfolioItem.nodes;
+    `) */
+/* const projects = data.allContentfulPortfolioItem.nodes; */
     return (
         <Layout>
             <h1>Homepage</h1>
-            <ul>
-                {projects.map((project) => (
-                    <li key={project.slug}>
-                        <Link to={`/portfolio/${project.slug}`}>{project.title}</Link>
-                    </li>
-                ))}
-            </ul>
+
         </Layout>
     )
 }
