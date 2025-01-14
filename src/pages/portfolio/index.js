283e3acfdfb8d986862d5b2a615b720bcb9f0fa9
HannaL <hanna.leinonen@iths.se>
2025-01-14 14:09:09 +0100

    Added content and style to portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,39 +1,56 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { Link, graphql } from 'gatsby'
-
-const PortfolioPage = ({ data }) => {
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { portfolioItems, portfolioItem, imageWrapper, divider, heading } from "../../styles/portfolio.module.css"
+const PortfolioPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allContentfulPortfolioItem {
+                nodes {
+                    title
+                    slug
+                    image {
+                        gatsbyImage(
+                            layout: CONSTRAINED,
+                            width: 330,
+                            height: 700
+                            )
+                    }
+                }
+            }
+        }
+    `)
     const items = data.allContentfulPortfolioItem.nodes
 
     return (
         <Layout>
-            <div>
-                <h1>Portfolio page</h1>
-                <ul>
-                    {items.map((item) => (
-                        <li key={item.slug}>
-                            <Link to={`/portfolio/${item.slug}`}>
-                                {item.title}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+            <div >
+                <h1 className={heading}>Projects</h1>
+                <div className={divider}></div>
+                <div className={portfolioItems}>
+                    {/* Iterera över items och rendera bild + länk */}
+                    {items.map((item) => {
+                        const image = getImage(item.image)
+                        return (
+                            <article key={item.slug} className={portfolioItem}>
+                                <h2>
+                                    <Link to={`/portfolio/${item.slug}`}>
+                                        {item.title}
+                                    </Link>
+                                    <div className={divider}></div>
+                                </h2>
+                                <GatsbyImage className={imageWrapper} image={image} alt={`Image of ${item.title}`} />
+                            </article>
+                        )
+                    })}
+                </div>
             </div>
         </Layout>
     )
 }
 
-export const query = graphql`
-    query {
-        allContentfulPortfolioItem {
-            nodes {
-                title
-                slug
-            }
-        }
-    }
-`
 export default PortfolioPage
 
 export const Head = () => <Seo title="Portfolio"></Seo>
